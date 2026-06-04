ccc92dead917fc4b8317e081cf1408895af5f322
djebsoft <djebbour1985@gmail.com>
2026-06-04 17:52:20 +0100

    refactor formatDate() to consider bookmarks added the same day

--- src/utils.js
+++ src/utils.js
@@ -13,6 +13,8 @@ export function formatDate(date) {
 		year: "numeric",
 		month: "long",
 		day: "numeric",
+		hour: "2-digit",
+		minute: "2-digit",
 	};
 	return new Date(date).toLocaleDateString(undefined, options);
 }
